78258bfcd61cea53baf0893a15f1f44063a3e795
Rafael de Freitas <167362524+devfreitas@users.noreply.github.com>
2026-05-02 06:49:53 -0300

    Delete CP2 directory

--- CP2/CP2/DTOs/LocacaoRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CP2.DTOs;
-
-public class LocacaoRequestDTO
-{ 
-        public int CarroId { get; set; }
-        public DateTime DataInicio { get; set; }
-        public DateTime DataFim { get; set; }
-}
